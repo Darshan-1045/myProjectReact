06858ab04e4dd30da4163f384cd4d306da54f7b8
Darshan-1045 <darshandarshanp10@gmail.com>
2026-03-17 11:00:06 +0530

    add Components

---
+++ 02-Components/src/components/card.jsx
@@ -0,0 +1,14 @@
+
+const card = () => {
+  return (
+    <div className="card">
+      <div>DARSHAN</div>
+      <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default card
---
+++ 02-Components/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        this is the navbar
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
